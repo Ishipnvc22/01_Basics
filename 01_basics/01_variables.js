edc146abec144968bd6a624ab512d04955f2cbfc
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-06 06:54:06 +0000

    Container Config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId  = 144553
+let accountEmail = "internshipnvc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+
+//accountId = 2 // not allow as it already got declared with const keyword
+
+console.log(accountId);
