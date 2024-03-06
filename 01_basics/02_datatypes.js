ab7a9b162a1f7f27b4933ce2ecea9909ebdda696
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-06 10:30:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using node js not browser
+console.log(3+3); // code readability should be high
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53 upto values
+// bigint
+// string => "",''
+// boolean => true,false
+// null => stand alone value
+// undefined => when u declare variable but not have declare it's value.
+// symbol => unique
+
+
+// object
+console.log(typeof age);
