a9e72808deb5e364eb44bdc05a6a41d18d95ea80
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-06 07:24:42 +0000

    Container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,18 @@ let accountEmail = "internshipnvc@gmail.com"
 var accountPassword = "12345"
 accountCity = "Surat"
 
+// when u declare a variable without value then js will consider it as undefined.
+
 //accountId = 2 // not allow as it already got declared with const keyword
 
 console.log(accountId);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope.
+*/
+
+accountEmail = "hvhc@gmail.com" // 
+accountPassword = "21212121"
+accountCity = "Ahmedabad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
