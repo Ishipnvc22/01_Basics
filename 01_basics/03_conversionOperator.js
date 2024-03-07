f3c3465ae86d8f20dfda9342688a850582d48899
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-07 06:07:15 +0000

    conversion of operators

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof 33);
+console.log(typeof(score)); // method way to check type
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" => this will get easily converted to number 33
+// "33abc" => this will not convert into number but gives you NaN
+// "true" => If we use true as a value it will return 1.
+// "false" => If we use false as a value it will return 0.
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// if u use "" empty string it will return false as boolean type.
+// if u use "..." not an empty string it will return true as boolean type.
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
