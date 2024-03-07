ad047dba27eada2449b073d46885ebfc9c009a8a
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-07 09:33:16 +0000

    summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,10 @@
+// call by value
+// call by reference
+
+// Primitive
+// 7 types : String, Number, BigInt, Null, Undefined, Boolean, Symbol
+
+
+
+// reference (Non primitive)
+// Array, Objects, Functions
