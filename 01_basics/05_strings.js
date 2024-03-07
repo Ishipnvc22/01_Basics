bb93a64f3cee5afad876a2fc9ba0f799567b0600
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-07 13:10:15 +0000

    done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+// string is denoted by both single and double quote
+
+const name = "hitesh"
+const repoCount = 50 
+
+console.log(name + repoCount + " value ");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
