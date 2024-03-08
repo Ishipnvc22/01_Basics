18382ecda79159719fd8057fa384160f83b28cdc
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-08 07:52:25 +0000

    done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,6 +3,31 @@
 const name = "hitesh"
 const repoCount = 50 
 
-console.log(name + repoCount + " value ");
+console.log(name + repoCount + " value "); // outdated syntax
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // nowadays use back text
+
+// string
+
+const gameName = new String('hitesh-hc');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8 , 4) // you can only give negative values in slice.
+console.log(anotherString);
+
+const newStringOne = "  hitesh  "
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudary"
+
+console.log(url.replace('%20','-'))
