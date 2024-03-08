e8d0d97a02e0df9a27fc235dbeccd7878b447243
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-08 13:07:42 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -13,7 +13,6 @@ const gameName = new String('hitesh-hc');
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
-
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
