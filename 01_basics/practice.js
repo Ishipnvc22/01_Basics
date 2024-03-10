a8175b3bfa703adc0bf9e802824598b502c80734
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-10 13:27:57 +0000

    practice

---
+++ 01_basics/practice.js
@@ -0,0 +1,20 @@
+// practice
+
+let a = 20
+let b = "30"
+let c = "Rupees"
+
+console.log(a);
+console.log(typeof(a))
+
+console.log(b);
+console.log(typeof(b))
+
+console.log(c);
+console.log(typeof(c))
+
+console.log(`Hello if you mix ${a} and ${b} then it will be sum of 50 ${c}.`)
+
+const newNumber = Number(b)
+console.log(newNumber);
+console.log(typeof(newNumber))
