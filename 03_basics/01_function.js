b8ea2bfe2edce09c977613b3c7d6d1eb84e0f87e
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-12 09:17:31 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -23,3 +23,33 @@ function loginUserMessage(username){
 }
 
 console.log(loginUserMessage("Meet"))
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "Meet",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200 , 400 , 500 , 1000]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200 , 400 , 500 , 1000]))
