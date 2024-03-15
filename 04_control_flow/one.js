c1f5286cd80c9e4d730f2a4a969a8939c3b64bfa
Ishipnvc22 <162411087+Ishipnvc22@users.noreply.github.com>
2024-03-15 13:22:12 +0000

    one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,8 +1,29 @@
 // if
 
 const isUserloggedIn = true
+const temperature = 41
 
-if (isUserloggedIn){ // condition can be true or false 
+// if (2 != 3){ // condition can be true or false 
+//     console.log("Executed");
+// }
 
+if (temperature < 50){
+     console.log("Less than 50");
+}
+else{
+    console.log("Temparature is greater than 50");
 }
 
+
+const score = 200
+
+if (score > 100){
+    const power = "Fly"
+    console.log(`User Power : ${power}`);
+}
+// console.log(`User Power: ${power}`);
+
+const balance = 1000
+
+if(balance>500) console.log("test"),
+console.log("test");
